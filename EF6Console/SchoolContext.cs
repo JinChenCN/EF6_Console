d16ed93b93d5d4ae91c9bca6af3d11ffd84eed41
Jin Chen <chenjin10@gmail.com>
2016-08-24 11:31:36 +1200

    Student and Standard tables were generated sucessfully.

---
+++ EF6Console/SchoolContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Console
+{
+   public class SchoolContext : DbContext
+    {
+        public SchoolContext() { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+    }
+}
